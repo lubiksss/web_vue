354b0066cce834639d37ea24590cf3362783e8f8
jake.0208 <jake.0208@kakaocorp.com>
2022-03-12 13:23:03 +0900

    Test Free version * data binding * v-for * event handler * v-if & modal window * import/export

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="isOpened == true" class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="isOpened=false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="item in menu" :key="item">
+      {{ item }}
+    </a>
+  </div>
+
+  <div v-for="data in datas" :key="data.id">
+    <img :src="data.image" alt="" class="room-img">
+    <h4 @click="isOpened = true">{{ data.title }}</h4>
+    <!--    <h3>{{ data.content }}</h3>-->
+    <p>{{ data.price }}원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import datas from './oneroom';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      datas: datas,
+      isOpened: false,
+      신고수: [0, 0, 0],
+      menu: ['Home', 'Products', 'About'],
+      products: ['역삼동 원룸', '천호동 원룸', '강북구 원룸']
+    }
+  },
+  methods: {
+    increase(i) {
+      this.신고수[i] += 1;
+    }
+  },
+  components: {}
 }
 </script>
 
 <style>
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  font-size: 20px;
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 </style>
